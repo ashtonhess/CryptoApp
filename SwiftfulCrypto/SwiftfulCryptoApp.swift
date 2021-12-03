c52d1d67f7c1d27ab0913fcc1956b69ad63fa9c0
ashtonhess <ashton.hess@yahoo.com>
2021-12-02 23:25:07 -0600

    Initial Commit

---
+++ SwiftfulCrypto/SwiftfulCryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulCryptoApp.swift
+//  SwiftfulCrypto
+//
+//  Created by Ashton Hess on 12/2/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulCryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

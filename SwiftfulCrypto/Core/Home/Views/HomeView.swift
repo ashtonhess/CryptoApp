a882071f31e01d93d63d8bb7d97a53334614d11b
ashtonhess <ashton.hess@yahoo.com>
2021-12-04 20:15:17 -0600

    Finishing setting up the home view navigation header. -switch between portfolio and live prices -animates custom circle buttons when switching between views -created extension part of HomeView.swift to more easily reference different Views, ex: moved an HStack to extensions and now it can be referenced by just (Hstack name) in the body of the view. Makes code a lot cleaner.

--- SwiftfulCrypto/Core/Home/Views/HomeView.swift
+++ SwiftfulCrypto/Core/Home/Views/HomeView.swift
@@ -18,26 +18,15 @@ struct HomeView: View {
                 .ignoresSafeArea()
             //content layer
             VStack{
+                //SEE EXTENSIONS BELOW
                 //This HStack is the Custom Navigation Header.
-                HStack{
-                    CircleButtonView(iconName: "info")
-                    Spacer()
-                    Text("Live Prices")
-                        .font(.headline)
-                        .fontWeight(.heavy)
-                        .foregroundColor(Color.theme.accent)
-                    Spacer()
-                    CircleButtonView(iconName: "chevron.right")
-                        .rotationEffect(Angle(degrees: showPortfolio ? 180 : 0))
-                        .onTapGesture{
-                            withAnimation(.spring()){
-                                showPortfolio.toggle()
-                            }
-                                
-                        }
-                }
-                .padding(.horizontal)
+                //Circle button, spacer, header text, spacer, circle button
+                homeHeader
                 
+                //This spacer makes the nav header go to the top.
+                //Min length spacer can be shrunk to, along the axis or axes of expansion.
+                //spacer cannot be shrunk to less than 0 length.
+                //will serve max length possible
                 Spacer(minLength: 0)
             }
         }
@@ -51,5 +40,46 @@ struct HomeView_Previews: PreviewProvider {
                 .navigationBarHidden(true)
         }
         
+    }
+}
+
+
+//this extension is an extension of this very class.
+//First, we are moving the HStack for the header to the extension.
+//This makes it so we can just use the name homeHeader to refer to this HStack.
+//cleans up the body of theis view alot.
+extension HomeView{
+    private var homeHeader: some View{
+        HStack{
+            
+            //If we are showing portfolio, this button icon is plus. If we are not showing portfolio, this button icon is info.
+            CircleButtonView(iconName: showPortfolio ? "plus" : "info")
+                .animation(.none, value: showPortfolio)
+                //.none is the animation that applys.
+                //value is the value to monitor for changes. If the value changes, this animation applys.
+                .background(CircleButtonAnimationView(animate: $showPortfolio))
+            
+            Spacer()
+            Text(showPortfolio ? "Portfolio":"Live Prices")
+                .font(.headline)
+                .fontWeight(.heavy)
+                .foregroundColor(Color.theme.accent)
+                .animation(.none, value: showPortfolio)
+            Spacer()
+            CircleButtonView(iconName: "chevron.right")
+                //adding ability for the icon to turn depending on if showPortfolio is true or not
+                //if true, angle is 180, else, angle is 0
+                .rotationEffect(Angle(degrees: showPortfolio ? 180 : 0))
+                //listener for a tap on the icon
+                .onTapGesture{
+                    withAnimation(.spring()){
+                        //when icon is tapped, showPortfolio toggled.
+                        //when showPortfolio toggled, .rotationEffect changes the rotation of icon.
+                        showPortfolio.toggle()
+                    }
+                        
+                }
+        }
+        .padding(.horizontal)
     }
 }

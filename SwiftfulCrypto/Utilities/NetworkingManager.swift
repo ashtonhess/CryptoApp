02cb0b463f1fb8810fd29ca541ee0df63b27773d
ashtonhess <ashton.hess@yahoo.com>
2022-01-14 19:41:05 -0600

    Networking Manager is finished. -downloads data from a URL -handles response and checks for errors -Throws custom error messages -Also handles the completion of a download.
    All of this makes working with the online data much cleaner everywhere else and provides a portable class to download any data from a URL.

--- SwiftfulCrypto/Utilities/NetworkingManager.swift
+++ SwiftfulCrypto/Utilities/NetworkingManager.swift
@@ -10,8 +10,21 @@ import Combine
 
 class NetworkingManager{
     
-    static func download(url: URL) -> AnyPublisher<Data, Error>{
+    //enum for custom errors to be called easily.
+    enum NetworkingError: LocalizedError{
         
+        case badURLResponse(url: URL)
+        case unknown
+        
+        var errorDescription: String? {
+            switch self{
+            case .badURLResponse(url: let url): return "[🚨] Bad response from URL: \(url)"
+            case .unknown: return "[🚨] Unknown error occured."
+            }
+        }
+    }
+    
+/**download function in NetworkingManager
         //Replaced type
         //Publishers.ReceiveOn<Publishers.TryMap<Publishers.SubscribeOn<URLSession.DataTaskPublisher, DispatchQueue>, Data>, DispatchQueue>
         //With type
@@ -19,7 +32,7 @@ class NetworkingManager{
         
         //To do this:
         // 1. Make AnyPublisher<Data, Error> the return type of the func.
-        //2. Put .eraseToAnyPublisher() on the func itself. 
+        //2. Put .eraseToAnyPublisher() on the func itself.
         
         //Original func:
         
@@ -30,19 +43,39 @@ class NetworkingManager{
         //That is the type that this function returns.
         
         //let temp = URLSession.shared...
-        
+ **/
+    static func download(url: URL) -> AnyPublisher<Data, Error>{
         return URLSession.shared.dataTaskPublisher(for: url)
             .subscribe(on: DispatchQueue.global(qos: .default))
-        //Checking for a valid response, if data is good or not.
-            .tryMap { (output) -> Data in
-                guard let response = output.response as? HTTPURLResponse,
-                        response.statusCode>=200 && response.statusCode < 300 else { //200 range for statusCodes mean successful response
-                            throw URLError(.badServerResponse)
-                        }
-                return output.data
-            }
+            .tryMap({ try handleURLResponse(output: $0, url: url)})
             .receive(on: DispatchQueue.main)
             //
             .eraseToAnyPublisher()
+    }
+    
+    //Checking for a valid response, if data is good or not.
+    static func handleURLResponse(output: URLSession.DataTaskPublisher.Output, url: URL) throws -> Data {
+        
+        //testing error Throw
+        //throw NetworkingError.badURLResponse(url: url)
+
+        guard let response = output.response as? HTTPURLResponse,
+                response.statusCode>=200 && response.statusCode < 300 else { //200 range for statusCodes mean successful response
+                    //This was Throwing a generic error, replaced with better error messages.
+//                    throw URLError(.badServerResponse)
+                    
+                    throw NetworkingError.badURLResponse(url: url)
+                }
+        return output.data
+    }
+    
+    static func handleCompletion(completion: Subscribers.Completion<Error>){
+        switch completion{
+        case .finished:
+            break
+        //If there is an error it will print here
+        case .failure(let error):
+            print(error.localizedDescription)
+        }
     }
 }
